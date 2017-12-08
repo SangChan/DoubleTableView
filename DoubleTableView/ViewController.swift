72a0e6cb571cd4da52bac9f728923c73add7222f
SangChan Lee <sangchan82@gmail.com>
2017-12-08 22:36:11 +0800

    implement dual table view

--- DoubleTableView/ViewController.swift
+++ DoubleTableView/ViewController.swift
@@ -20,9 +20,15 @@ class ViewController: UIViewController {
         "4" : [ "4.0" ]
     ]
     
+    var selectedRow : Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        subTableView.dataSource = self
+        subTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,11 +45,35 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         if tableView.tag == 0 {
             return dataModel.keys.count
         }
-        return 0
+        guard let row = selectedRow else { return 0 }
+        let array = dataModel.keys.sorted()
+        let key = array[row]
+        guard let selectedArray = dataModel[key] else { return 0 }
+        return selectedArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        if tableView.tag == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "leftCell")
+            cell?.textLabel?.text = dataModel.keys.sorted()[indexPath.row]
+            return cell!
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rightCell")
+        cell?.textLabel?.text = "wow"
+        guard let row = selectedRow else { return cell! }
+        let array = dataModel.keys.sorted()
+        let key = array[row]
+        guard let selectedArray = dataModel[key] else { return cell! }
+        cell?.textLabel?.text = selectedArray[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.tag == 0 {
+            self.selectedRow = indexPath.row
+            self.subTableView.reloadData()
+        }
+        
     }
 }
 
